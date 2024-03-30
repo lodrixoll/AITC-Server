cf9a3194ed825084725fb68dad7df25ce6df6fb6
altgrowth-liam <liam@altgrowth.org>
2024-03-30 14:12:11 -0700

    Add error handling for MongoDB connection

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3001;
 
+// MongoDB connection
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('MongoDB connected...'))
-  .catch(err => console.log(err));
+  .then(() => {
+    console.log('MongoDB connected...');
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+    console.error("exiting app");
+    process.exit(1); // Stop the app if the DB connection fails
+  });
 
 app.use(express.json());
 
