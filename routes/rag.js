9c8a6243349f874ccef8db0f6deb88610402765b
altgrowth-liam <liam@altgrowth.org>
2024-04-01 21:29:13 -0700

    add rag on pdfs

--- routes/rag.js
+++ routes/rag.js
@@ -1,74 +1,69 @@
 const express = require('express');
 const router = express.Router();
-
-const cheerio = require("cheerio");
-const { CheerioWebBaseLoader } = require("langchain/document_loaders/web/cheerio");
+const { PDFLoader } = require("langchain/document_loaders/fs/pdf");
 const { RecursiveCharacterTextSplitter } = require("langchain/text_splitter");
 const { MemoryVectorStore } = require("langchain/vectorstores/memory");
 const { OpenAIEmbeddings, ChatOpenAI } = require("@langchain/openai");
-const { pull } = require("langchain/hub");
-const { ChatPromptTemplate } = require("@langchain/core/prompts");
 const { StringOutputParser } = require("@langchain/core/output_parsers");
+const { pull } = require("langchain/hub");
 const { createStuffDocumentsChain } = require("langchain/chains/combine_documents");
 
 require('dotenv').config();
 
-// Chat endpoint
+
+// rag endpoint
 router.post('/', async (req, res) => {
 
-    console.log("\n\n==== RAG Chat ====")
+    console.log("\n\n==== New RAG ====")
 
-    const loader = new CheerioWebBaseLoader(
-    "https://lilianweng.github.io/posts/2023-06-23-agent/"
-    );
-    console.log("Loader created")
-
+    // load docs
+    const loader = new PDFLoader("uploads/example.pdf");
     const docs = await loader.load();
-    console.log("Documents loaded")
+    console.log(`Loaded ${docs.length} documents`)
 
+    // split the documents into chunks
     const textSplitter = new RecursiveCharacterTextSplitter({
     chunkSize: 1000,
     chunkOverlap: 200,
     });
     const splits = await textSplitter.splitDocuments(docs);
-    console.log("Documents split")
 
+    // create a vector store from the splits
     const vectorStore = await MemoryVectorStore.fromDocuments(
     splits,
     new OpenAIEmbeddings()
     );
-    console.log("Vector store created")
 
     // Retrieve and generate using the relevant snippets of the blog.
-    const retriever = vectorStore.asRetriever();
-    console.log("Retriever created")
-
+    const retriever = vectorStore.asRetriever(75);
     const prompt = await pull("rlm/rag-prompt");
-    console.log("Prompt pulled")
+    const llm = new ChatOpenAI({ modelName: "gpt-4-turbo-preview", temperature: 0 });
 
-    const llm = new ChatOpenAI({ modelName: "gpt-3.5-turbo", temperature: 0 });
-    console.log("LLM created")
-
+    // create chain
     const ragChain = await createStuffDocumentsChain({
     llm,
     prompt,
     outputParser: new StringOutputParser(),
     });
-    console.log("RAG chain created")
 
+    // retrieve relevant docs
     const retrievedDocs = await retriever.getRelevantDocuments(
-    "what is task decomposition"
+      "Buyer, Seller, Landlord, Tenant, Agent, Real Estate Broker (Firm), Salesperson, Buyer's Brokerage Firm, Seller's Brokerage Firm, Date"
     );
-    console.log("Documents retrieved")
-
+    console.log(`Retrieved ${retrievedDocs.length} documents`)
+    
+    // perform the rag
     response = await ragChain.invoke({
-        question: "What is task decomposition?",
+        question: "What are the names of the people that are the Seller, " + 
+                  "Listing Agent, Listing Broker, Buyer, Buyer's Agent, " + 
+                  "Buyer's Broker, in this real estate purchase agreement? " + 
+                  "Your response will be used in production code therefore you are allowed to respond with plain JSON object only. Do not include any markdown or preceed your response with special characters.",
         context: retrievedDocs,
       });
     console.log(response);
-
-    // send response
-    res.json({ response: response });
+    
+    // send the response
+    res.json(JSON.parse(response));
 });
 
 module.exports = router;
