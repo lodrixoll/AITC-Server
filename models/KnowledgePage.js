7b7cbf35e438d0d62561d86b060702d5ca519592
altgrowth-liam <liam@altgrowth.org>
2024-04-09 18:21:04 -0700

    update filename & structure

--- models/KnowledgePage.js
+++ models/KnowledgePage.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
 const knowledgeSchema = new mongoose.Schema({
-    FileName: String,
-    PageNumber: Number,
-    Content: String,
+    documentTitle: String,
+    pageNumber: Number,
+    emptyContent: String,
+    completedContent: String,
+    complianceChecklist: String,
+    metaDescription: String
 }, { timestamps: true });
 
 module.exports = mongoose.model('Knowledge', knowledgeSchema);
