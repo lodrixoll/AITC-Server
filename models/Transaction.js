1c34909f416ebb60b5aac1c0ae4e6ce1f4ecc3f1
lodrixoll <lodrixoll@gmail.com>
2024-04-19 19:05:50 -0700

    add transactions

--- models/Transaction.js
+++ models/Transaction.js
@@ -2,12 +2,40 @@ const mongoose = require('mongoose');
 
 const transactionSchema = new mongoose.Schema({
     address: String,
-    MLS: String,
     price: String,
-    buyer: String,
-    closingDate: String,
-
+    Seller: String,
+    "Listing Agent": String,
+    "Listing Broker": String,
+    Buyer: String,
+    "Buyer's Agent": String,
+    "Buyer's Broker": String
 }, { timestamps: true });
 
 module.exports = mongoose.model('Transaction', transactionSchema);
 
+
+// UPDATED TRANSACTION SCHEMA
+// ------------------
+// Address: String
+// MLS #: String
+// Purchase Price: String
+// Closing Date: String
+// Seller: String
+// Buyer: String
+// Contacts: [contacts]
+// Task List: [tasks]
+// ------------------
+
+// UPDATED CONTACTS SCHEMA
+// ------------------
+// Name: String
+// Phone: String
+// Email: String
+// ------------------
+
+// UPDATED TASKS SCHEMA
+// ------------------
+// Status: String
+// Date: String
+// Description: String
+// ------------------
